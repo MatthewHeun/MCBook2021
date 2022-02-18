7f47c32ada97d2406a31106cd64a5f0e1a20cdb6
Matthew Heun <matthew.heun@me.com>
2022-02-18 13:01:08 -0500

    .bat files for JVA

---
+++ compile.bat
@@ -0,0 +1,20 @@
+
+"C:\Program Files\R\R-4.1.2\bin\Rscript.exe" knit_script.R
+
+pdflatex book.tex
+
+
+REM from https://stackoverflow.com/questions/8385454/batch-files-list-all-files-in-a-directory-with-relative-paths
+
+REM See also https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/forfiles
+
+forfiles /m *.aux /c "cmd /c bibtex @relpath"
+
+makeindex book.idx
+pdflatex book.tex
+pdflatex book.tex
+makeindex book.idx 
+pdflatex book.tex 
+open book.pdf
+
+REM ./clean.sh
