e43c9fdb63ad5443bec23a8879a4b7a24c41b765
Matthew Heun <matthew.heun@me.com>
2023-08-08 09:18:45 -0400

    Edited compileCore100.sh

--- compileCore100.sh
+++ compileCore100.sh
@@ -1,21 +1,18 @@
 
-"Rscript" knit_scriptCore100.R
+/usr/local/bin/Rscript knit_scriptCore100.R
 
 pdflatex Core100Summary.tex
 
 
-REM from https://stackoverflow.com/questions/8385454/batch-files-list-all-files-in-a-directory-with-relative-paths
-
-REM See also https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/forfiles
-
-forfiles /M *.aux /c "cmd /c bibtex @relpath"
+bibtex Core100Summary.aux
 
 makeindex Core100Summary.idx
 pdflatex Core100Summary.tex
 pdflatex Core100Summary.tex
 makeindex Core100Summary.idx 
 pdflatex Core100Summary.tex 
-start Core100Summary.pdf
 
-REM ./clean.sh
-.\cleanJVA.bat
+open Core100Summary.pdf
+
+# Comment the following line if it helps to see the auxiliary files.
+# ./clean.sh
